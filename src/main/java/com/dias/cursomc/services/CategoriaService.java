3b2655ec5904fd6c1acd613a569c7191176451a9
Matheus Dias <matheusdyas007@hotmail.com>
2022-01-29 18:37:54 -0300

    Ajustes no endpoint categorias/id e criacao do handler de excecoes

--- src/main/java/com/dias/cursomc/services/CategoriaService.java
+++ src/main/java/com/dias/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.dias.cursomc.domain.Categoria;
 import com.dias.cursomc.repositories.CategoriaRepository;
+import com.dias.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id); //findById adicionado pelo spring com versoes maiores que 2, retornar um obj Optional
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
