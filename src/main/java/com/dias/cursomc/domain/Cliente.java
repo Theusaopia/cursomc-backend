e9e58c676b77d2a3cb965430f6e151ef7f2bd62c
Matheus Dias <matheusdyas007@hotmail.com>
2022-02-21 20:29:41 -0300

    removido JsonManagedReference das classes de dominio e trocado o JsonBackReference por JsonIgnore

--- src/main/java/com/dias/cursomc/domain/Cliente.java
+++ src/main/java/com/dias/cursomc/domain/Cliente.java
@@ -15,8 +15,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.dias.cursomc.domain.enums.TipoCliente;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 public class Cliente implements Serializable{
@@ -30,7 +30,6 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
@@ -38,7 +37,7 @@ public class Cliente implements Serializable{
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>(); //caso especial onde NAO foi criado uma entidade para o telefone, mas ele necessita uma tabela no banco
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
