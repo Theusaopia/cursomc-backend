a8fbdfb8a040ce538b016e146cd7eaaaa0ca8b31
Matheus Dias <matheusdyas007@hotmail.com>
2022-02-25 21:35:54 -0300

    criado validacao sintatica com Bean Validation para POST e PUT de categorias

--- src/main/java/com/dias/cursomc/services/CategoriaService.java
+++ src/main/java/com/dias/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.dias.cursomc.domain.Categoria;
+import com.dias.cursomc.dto.CategoriaDTO;
 import com.dias.cursomc.repositories.CategoriaRepository;
 import com.dias.cursomc.services.exceptions.DataIntegrityException;
 import com.dias.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
