391ab6d3e92f962410b12ac5e3009e0c87ba5900
Matheus Dias <matheusdyas007@hotmail.com>
2022-03-01 12:22:16 -0300

    criado profile de desenvolvimento

---
+++ src/main/java/com/dias/cursomc/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.dias.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.dias.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.dll-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
