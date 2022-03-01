c05b5b9bc80ec3e306fd64f9f19d667f5bd027ed
Matheus Dias <matheusdyas007@hotmail.com>
2022-02-28 21:03:48 -0300

    criado endpoint POST para pedidos

--- src/main/java/com/dias/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/dias/cursomc/domain/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.dias.cursomc.domain;
 import javax.persistence.Entity;
 
 import com.dias.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
