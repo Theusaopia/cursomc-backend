bd54fa80ce4ff6d5bb3766a8cb10f1b7fb7a8485
Matheus Dias <matheusdyas007@hotmail.com>
2022-03-19 22:01:50 -0300

    implementado autenticação com JWT

---
+++ src/main/java/com/dias/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.dias.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.dias.cursomc.domain.Cliente;
+import com.dias.cursomc.repositories.ClienteRepository;
+import com.dias.cursomc.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		
+		if(cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
